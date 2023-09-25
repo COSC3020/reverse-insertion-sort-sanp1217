b80acc2305134923eb031020e87b2c00d9564344
pabs <pablosgrad12@gmail.com>
2023-09-24 18:07:02 -0600

    Adds code for reverse insertion sort

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function insertionSortReverse(array) {
+    //Start at second to last element
+    for (var i = array.length - 2; i > -1; i--) {
+        var val = array[i];
+        var j;
+
+        for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
+            array[j] = array[j + 1]
+        }
+        array[j] = val;
+    }
     return array;
 }
+
